292b8b839bf5e76a791e04e586e162efe0a32b01
koulp <vkoulp@gmail.com>
2017-10-14 16:03:15 +0300

    fixarismeno trexei to login

--- src/main/java/com/repair/car/RepairCarApplication.java
+++ src/main/java/com/repair/car/RepairCarApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.repair.car.*")
+@ComponentScan("com.repair.car")
 public class RepairCarApplication {
 
 	public static void main(String[] args) {
