1f2e1ae58957468b81ed746a16cede38af5d7516
vicalk <vicalkalais@gmail.com>
2017-10-14 11:48:35 +0300

    vic

---
+++ src/main/java/com/repair/car/Domain/User.java
@@ -0,0 +1,124 @@
+package com.repair.car.Domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.*;
+
+
+@Entity (name = "USER")
+public class User implements Serializable {
+    @Id
+    @Column(name = "USER_ID",nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+
+    @Column(name = "AFM")
+    private String afm;
+
+    @Column (name = "LASTNAME")
+    private String lastname;
+
+    @Column (name = "FIRSTNAME")
+    private String firstname;
+
+    @Column (name = "ADDRESS")
+    private String address;
+
+    @Column(name = "EMAIL",nullable = false)
+    private String email;
+
+    @Column(name = "PASSWORD",nullable = false)
+    private String password;
+
+    @Column(name = "USER_TYPE",nullable = false)
+    private int userType;
+
+    @OneToMany(mappedBy = "user", targetEntity = Vehicle.class)
+    private List<Vehicle> vehicles;
+
+
+    public User(String afm, String lastname, String firstname, String address, String email, String password, int userType) {
+        this.afm = afm;
+        this.lastname = lastname;
+        this.firstname = firstname;
+        this.address = address;
+        this.email = email;
+        this.password = password;
+        this.userType = userType;
+    }
+
+    public User() {
+    }
+
+    public String getAfm() {
+        return afm;
+    }
+
+    public void setAfm(String afm) {
+        this.afm = afm;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getUserType() {
+        return userType;
+    }
+
+    public void setUserType(int userType) {
+        this.userType = userType;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "afm=" + afm +
+                ", lastname='" + lastname + '\'' +
+                ", firstname='" + firstname + '\'' +
+                ", address='" + address + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", userType=" + userType +
+                '}';
+    }
+}
+
+
---
+++ src/main/java/com/repair/car/model/RegistrationForm.java
@@ -0,0 +1,91 @@
+package com.repair.car.model;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+public class RegistrationForm {
+
+    private static final String AFM_PATTERN = "^[0-9]*$";
+
+    private static final String PASSWORD_PATTERN = "^[a-zA-Z0-9@#$%^&]*$";
+
+    private static final String CHARACTERS_PATTERN = "^[a-zA-Z ]*$";
+
+    private static final String EMAIL_PATTERN = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{1,63}$";
+
+    private static final int PASSWORD_MINSIZE = 6;
+
+    private static final int AFM_SIZE = 9;
+
+
+    @Pattern(regexp = AFM_PATTERN, message = "{register.afm.invalid}")
+    @Size(min = AFM_SIZE, max = AFM_SIZE, message = "{register.afm.size}")
+    private int afm;
+
+    @Pattern(regexp = CHARACTERS_PATTERN, message = "{register.lastname.invalid}")
+    private String lastname;
+
+    @Pattern(regexp = CHARACTERS_PATTERN, message = "{register.firstname.invalid}")
+    private String firstname;
+
+    @Pattern(regexp = CHARACTERS_PATTERN, message = "{register.address.invalid}")
+    private String address;
+
+    @NotNull(message = "{register.email.null}")
+    @Pattern(regexp = EMAIL_PATTERN, message = "{register.email.invalid}")
+    private String email;
+
+    @NotNull(message = "{register.password.null}")
+    @Pattern(regexp = PASSWORD_PATTERN, message = "{register.password.invalid}")
+    @Size(min = PASSWORD_MINSIZE, message = "{register.password.size}")
+    private String password;
+
+    public int getAfm() {
+        return afm;
+    }
+
+    public void setAfm(int afm) {
+        this.afm = afm;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
