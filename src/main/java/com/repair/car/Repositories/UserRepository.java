32652c0f5cbc858491b787d233d0abf2c62d4699
vicalk <vicalkalais@gmail.com>
2017-10-14 14:51:00 +0300

    vic

---
+++ src/main/java/com/repair/car/Repositories/UserRepository.java
@@ -0,0 +1,22 @@
+package com.repair.car.Repositories;
+
+
+import com.repair.car.Domain.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Page<User> findAll(Pageable pageable);
+
+    User findByEmail(String email);
+    User findByEmailAndPassword(String email, String password);
+
+    User save(User user);
+
+}
+
+
+
---
+++ src/main/java/com/repair/car/Services/UserDetailsServiceImpl.java
@@ -0,0 +1,34 @@
+package com.repair.car.Services;
+
+import com.repair.car.Domain.User;
+import com.repair.car.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        if (user.getUserType()== 1 )
+            grantedAuthorities.add(new SimpleGrantedAuthority("ADMIN"));
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), grantedAuthorities);
+    }
+}
