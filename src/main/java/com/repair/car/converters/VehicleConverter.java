86cde7a17b7e933c3307fb6c9a48e117a2b66781
vicalk <vicalkalais@gmail.com>
2017-10-23 21:26:56 +0300

    login ok

--- src/main/java/com/repair/car/converters/VehicleConverter.java
+++ src/main/java/com/repair/car/converters/VehicleConverter.java
@@ -1,36 +1,36 @@
-package com.repair.car.converters;
-
-import com.repair.car.domain.Vehicle;
-import com.repair.car.model.VehicleDetails;
-import com.repair.car.model.VehicleRegisterForm;
-
-public class VehicleConverter {
-
-    public static Vehicle buildVehicleObject(VehicleRegisterForm vehicleRegisterForm) {
-        Vehicle vehicle = new Vehicle();
-        vehicle.setPlateNo(vehicleRegisterForm.getPlateNo());
-        vehicle.setCarModel(vehicleRegisterForm.getCarModel());
-        vehicle.setColor(vehicleRegisterForm.getColor());
-        vehicle.setYear(vehicleRegisterForm.getYear());
-        vehicle.setAfm(vehicleRegisterForm.getAfm());
-
-
-        return vehicle;
-    }
-
-
-
-        public static VehicleDetails buildVehicleDetails(Vehicle vehicle) {
-            VehicleDetails vehicleDetails = new VehicleDetails();
-            vehicleDetails.setPlateNo(vehicle.getPlateNo());
-            vehicleDetails.setCarModel(vehicle.getCarModel());
-            vehicleDetails.setYear(vehicle.getYear());
-            vehicleDetails.setColor(vehicle.getYear());
-            vehicleDetails.setAfm(vehicle.getYear());
-            return vehicleDetails;
-
-    }
-
-}
-
-
+//package com.repair.car.converters;
+//
+//import com.repair.car.domain.Vehicle;
+//import com.repair.car.model.VehicleDetails;
+//import com.repair.car.model.VehicleRegisterForm;
+//
+//public class VehicleConverter {
+//
+//    public static Vehicle buildVehicleObject(VehicleRegisterForm vehicleRegisterForm) {
+//        Vehicle vehicle = new Vehicle();
+//        vehicle.setPlateNo(vehicleRegisterForm.getPlateNo());
+//        vehicle.setCarModel(vehicleRegisterForm.getCarModel());
+//        vehicle.setColor(vehicleRegisterForm.getColor());
+//        vehicle.setYear(vehicleRegisterForm.getYear());
+//        vehicle.setAfm(vehicleRegisterForm.getAfm());
+//
+//
+//        return vehicle;
+//    }
+//
+//
+//
+//        public static VehicleDetails buildVehicleDetails(Vehicle vehicle) {
+//            VehicleDetails vehicleDetails = new VehicleDetails();
+//            vehicleDetails.setPlateNo(vehicle.getPlateNo());
+//            vehicleDetails.setCarModel(vehicle.getCarModel());
+//            vehicleDetails.setYear(vehicle.getYear());
+//            vehicleDetails.setColor(vehicle.getYear());
+//            vehicleDetails.setAfm(vehicle.getYear());
+//            return vehicleDetails;
+//
+//    }
+//
+//}
+//
+//
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,12 @@
-insert into users(USER_ID,AFM,LASTNAME,FIRSTNAME,ADDRESS,EMAIL,PASSWORD,user_type) values ('1','123456789','Doe','John','skoufa 26','john@mail.com','pass1234','ADMIN');
+insert into users(AFM,LASTNAME,FIRSTNAME,ADDRESS,EMAIL,PASSWORD,user_type) values ('123456789','Doe','John','skoufa 26','john@mail.com','pass1234','ADMIN');
 insert into users(AFM,LASTNAME,FIRSTNAME,ADDRESS,EMAIL,PASSWORD,user_type) values ('145678923','Doe','Jane','skoufa 26','jane@mail.com','pass1234','SIMPLE');
 
---insert into users(USER_ID,AFM,FIRSTNAME,LASTNAME,email,PASSWORD,user_type) values ('2','23','Jane','Doe','jane@mail.com','pass1234','SIMPLE');
---insert into users(USER_ID,AFM,FIRSTNAME,LASTNAME,email,PASSWORD,user_type) values ('1','12','John','Doe','john@mail.com','pass1234','ADMIN');
---insert into users(USER_ID,AFM,FIRSTNAME,LASTNAME,email,PASSWORD,user_type) values ('2','23','Jane','Doe','jane@mail.com','pass1234','SIMPLE');
+insert into vehicles(PLATENO,CAR_MODEL,YEAR,COLOR,AFM,USER_ID) values ('IMA-8357','AUDI','2007','GREY','123456789','1');
+insert into vehicles(PLATENO,CAR_MODEL,YEAR,COLOR,AFM,USER_ID) values ('IMA-8356','AUDI','2007','GREY','145678923','2');
+
+insert into repairs(REPAIR_DATE,REPAIR_STATUS,REPAIR_TYPE,REPAIR_COST,REPAIR_DESCRIPTION,VEHICLE_ID) values ('2007-10-11','IN PROGRESS','SMALL','500','OIL CHANGE',1);
+insert into repairs(REPAIR_DATE,REPAIR_STATUS,REPAIR_TYPE,REPAIR_COST,REPAIR_DESCRIPTION,VEHICLE_ID) values ('2007-11-11','COMPLETED','LARGE','1000','FILTER CHANGE',2);
+
+
+--insert into parts(USER_ID,AFM,FIRSTNAME,LASTNAME,email,PASSWORD,user_type) values ('2','23','Jane','Doe','jane@mail.com','pass1234','SIMPLE');
+
