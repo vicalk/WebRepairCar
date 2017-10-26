c69df997d3db7ca4c509f2c25cd4dc0a6d18a928
vicalk <32147158+vicalk@users.noreply.github.com>
2017-10-26 13:53:46 +0300

    insert statements

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-insert into users(USER_ID,AFM,FIRSTNAME,LASTNAME,EMAIL,PASSWORD,ROLE,ADDRESS) values ('1','12','John','Doe','john@mail.com','pass','ADMIN','Baker Street');
-insert into users(USER_ID,AFM,FIRSTNAME,LASTNAME,EMAIL,PASSWORD,ROLE,ADDRESS) values ('2','23','Jane','Doe','jane@mail.com','pass','SIMPLE','Beef Street');
+insert into users(USER_ID,AFM,FIRSTNAME,LASTNAME,EMAIL,PASSWORD,ROLE,ADDRESS) values ('1','123456987','John','Doe','john@mail.com','pass','ADMIN','Baker Street');
+insert into users(USER_ID,AFM,FIRSTNAME,LASTNAME,EMAIL,PASSWORD,ROLE,ADDRESS) values ('2','231456789','Jane','Doe','jane@mail.com','pass','SIMPLE','Beef Street');
 insert into users(USER_ID,AFM,FIRSTNAME,LASTNAME,EMAIL,PASSWORD,ROLE,ADDRESS) values ('3','987456123','Teo','Dora','teo@mail.com','pass345','SIMPLE','Teo Street');
 insert into users(USER_ID,AFM,FIRSTNAME,LASTNAME,EMAIL,PASSWORD,ROLE,ADDRESS) values ('4','123456789','George','Luke','George@mail.com','pass1234','SIMPLE','Bar Street');
 
